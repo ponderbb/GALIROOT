depth added, normalization might be wrong

src/loader.py
@@ -20,11 +20,15 @@ class KeypointsDataset(Dataset):
     Loader for supervisely based dataset
     '''
 
-    def __init__(self, config, annotations, images, transform):
+    def __init__(self, config, annotations, images, masks, transform, depth=False):
         self.config = config
         self.annotations_list = annotations
         self.images_list = images
+        self.mask_list = masks
         self.transform = transform
+        self.img_norm = A.Compose([A.Normalize(mean=[0.3399, 0.3449, 0.1555], std=[0.1296, 0.1372, 0.1044])])
+        self.mask_norm = A.Compose([A.Normalize(mean=[180.62], std=[47.2969])])
+        self.depth_channel = depth
 
 
     def __len__(self):
@@ -36,11 +40,26 @@ class KeypointsDataset(Dataset):
         if not annotation['objects']:
             raise Exception
         else:
+            # Read in values
             kp_raw = annotation['objects'][0]['points']['exterior'][0] # TODO: add option for reading in multipoint 
             keypoints = [(kp_raw[0],kp_raw[1])] # tuple wrapped in a list
+
             image = cv2.imread(self.images_list[idx])
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            data = self.transform(image=image, keypoints=keypoints)
+            norm_img = self.img_norm(image=image)
+
+            if self.depth_channel:
+                mask = np.asarray(Image.open(self.mask_list[idx]))
+                norm_mask = self.mask_norm(image=mask)
+                norm_mask = np.expand_dims(norm_mask['image'], axis=-1)
+
+                # Concatenate into a 4 channel input
+                combined_image = np.concatenate((norm_img['image'], norm_mask), axis=2)
+
+                data = self.transform(image=combined_image, keypoints=keypoints)
+            else:
+                data = self.transform(image=norm_img['image'], keypoints=keypoints)
+
             if not data['keypoints']:
                 print('Empty keypoint')
                 pass
@@ -80,9 +99,10 @@ def generate_transform(config):
         valid_transform.append(A.Crop(x_min=x[0], y_min=y[0], x_max=x[1], y_max=y[1]))
 
         # add augmentations if specified in the .json
-        if add_augmentation['normalization']:
-            train_transform.append(A.Normalize(mean=[0.3399, 0.3449, 0.1555], std=[0.1296, 0.1372, 0.1044]))
-            valid_transform.append(A.Normalize(mean=[0.3399, 0.3449, 0.1555], std=[0.1296, 0.1372, 0.1044]))
+        if add_augmentation['normalization']: # TODO: remove this from config
+            # train_transform.append(A.Normalize(mean=[0.3399, 0.3449, 0.1555], std=[0.1296, 0.1372, 0.1044]))
+            # valid_transform.append(A.Normalize(mean=[0.3399, 0.3449, 0.1555], std=[0.1296, 0.1372, 0.1044]))
+            pass
 
         if add_augmentation['gaussian_blur']:
             train_transform.append(A.GaussianBlur())

src/mean_std_dataset.py
@@ -1,5 +1,3 @@
-
-
 from numpy.core import numeric
 from torch.utils.data.dataset import Dataset
 import torchvision.datasets as datasets
@@ -37,12 +35,14 @@ class DepthTransform(Dataset):
 
     def __getitem__(self, idx):
         img = np.asarray(Image.open(self.img_list[idx]))
+        img = ((img-300)*255)/((500-300))
+        img = img.clip(min=0, max=255)
         data = self.transforms(img)
         return data
 
 if __name__ == "__main__":
 
-    data_path ="/home/bbejczy/repos/GALIROOT/data/l515_lab_1410/depth/"
+    data_path ="/zhome/3b/d/154066/repos/GALIROOT/data/l515_lab_1410/depth"
 
     # Train dataset
     a_transform = A.Compose([
@@ -72,7 +72,6 @@ if __name__ == "__main__":
     # print(std)
 
     mean = 0.
-
     std = 0.
     nb_samples = 0
     for data in loader:

src/models.py
@@ -6,6 +6,7 @@ import numpy as np
 
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
 import pretrainedmodels
 
 import utils
@@ -24,7 +25,7 @@ class SelfNet(nn.Module):
 
         # output = (W-F+2P)/S+1
 
-        self.conv1 = nn.Conv2d(3, 32, 5) # 315 385
+        self.conv1 = nn.Conv2d(4, 32, 5) # 315 385
         self.conv2 = nn.Conv2d(32, 64, 3) # 156 191
         self.conv3 = nn.Conv2d(64, 128, 3) # 77 94
         self.conv4 = nn.Conv2d(128, 256, 3) # 37 46
@@ -60,9 +61,9 @@ class SelfNet(nn.Module):
     def forward(self, x):
         # x = x.type(torch.FloatTensor)
         x = x.float()
-        x = self.pool(F.relu(self.norm1(self.conv1(x))))
-        x = self.pool(F.relu(self.norm2(self.conv2(x))))
-        x = self.pool(F.relu(self.norm3(self.conv3(x))))
+        x = self.dropout(self.pool(F.relu(self.norm1(self.conv1(x)))))
+        x = self.dropout(self.pool(F.relu(self.norm2(self.conv2(x)))))
+        x = self.dropout(self.pool(F.relu(self.norm3(self.conv3(x)))))
         x = self.pool(F.relu(self.norm4(self.conv4(x))))
         x = self.pool(F.relu(self.norm5(self.conv5(x))))
 
@@ -283,7 +284,7 @@ class ResNet18(nn.Module):
         self.l7 = nn.Linear(16,2)
 
         self.dropout = nn.Dropout(p=0.25)
-    def forward(self, x):
+    def forward(self, x): 
         # get the batch size only, ignore (c, h, w)
         batch, _, _, _ = x.shape
         # x = x.permute(0,3,1,2)
@@ -297,7 +298,6 @@ class ResNet18(nn.Module):
         x = self.l7(x)
 
         return x.type(torch.float64)
-
 
 class ResNet18_v2(nn.Module):
     def __init__(self, pretrained, requires_grad):
@@ -325,14 +325,66 @@ class ResNet18_v2(nn.Module):
         self.l9 = nn.Linear(4,2)
         
         self.dropout = nn.Dropout(p=0.25)
-        self.norm2 = nn.BatchNorm1d(256)
-        self.norm3 = nn.BatchNorm1d(128)
-        self.norm4 = nn.BatchNorm1d(64)
+        # self.norm2 = nn.BatchNorm1d(256)
+        # self.norm3 = nn.BatchNorm1d(128)
+        # self.norm4 = nn.BatchNorm1d(64)
     def forward(self, x):
         # get the batch size only, ignore (c, h, w)
         batch, _, _, _ = x.shape
         # x = x.permute(0,3,1,2)
         x = self.model.features(x)
+        x = F.adaptive_avg_pool2d(x, 1).reshape(batch, -1)
+        x = F.relu(self.l2(x))
+        x = F.relu(self.l3(x))
+        x = F.relu(self.l4(x))
+        x = self.l5(x)
+        x = self.l6(x)
+        x = self.l7(x)
+        x = self.l8(x)
+        x = self.l9(x)
+
+        return x.type(torch.float64)
+
+class ResNet18_4ch(nn.Module):
+    def __init__(self, pretrained, requires_grad):
+        super(ResNet18_4ch, self).__init__()
+        # if pretrained == True:
+        #     self.model = pretrainedmodels.__dict__['resnet18'](pretrained='imagenet')
+        # else:
+        #     self.model = pretrainedmodels.__dict__['resnet18'](pretrained=None)
+        # if requires_grad == True:
+        #     for param in self.model.parameters():
+        #         param.requires_grad = True
+        #     print('Training intermediate layer parameters...')
+        # elif requires_grad == False:
+        #     for param in self.model.parameters():
+        #         param.requires_grad = False
+        #     print('Freezing intermediate layer parameters...')
+        self.model = models.resnet18(pretrained=pretrained)
+
+        # add own layer
+        pretrained_weights = self.model.conv1.weight.clone()
+        self.model.conv1 = nn.Conv2d(4, 64, kernel_size=7, stride=2, padding=3,bias=False)
+        with torch.no_grad():
+            self.model.conv1.weight[:,:3,:,:] = torch.nn.Parameter(pretrained_weights)
+            self.model.conv1.weight[:,3,:,:] = torch.nn.Parameter(pretrained_weights[:,1,:,:])#
+
+        self.new_model = nn.Sequential(*list(self.model.children())[:-2]) # remove the last two layers 
+
+        # change the final layer
+        self.l2 = nn.Linear(512, 256)
+        self.l3 = nn.Linear(256, 128)
+        self.l4 = nn.Linear(128, 64)
+        self.l5 = nn.Linear(64,32)
+        self.l6 = nn.Linear(32,16)
+        self.l7 = nn.Linear(16,8)
+        self.l8 = nn.Linear(8,4)
+        self.l9 = nn.Linear(4,2)
+
+    def forward(self, x):
+        # get the batch size only, ignore (c, h, w)
+        batch, _, _, _ = x.shape
+        x = self.new_model(x)
         x = F.adaptive_avg_pool2d(x, 1).reshape(batch, -1)
         x = F.relu(self.l2(x))
         x = F.relu(self.l3(x))
@@ -361,7 +413,8 @@ models_list = [SelfNet(),
                FaceKeypointResNet50(pretrained=True, requires_grad=True),
                FaceKeypointResNet50_dropout(pretrained=True, requires_grad=True),
                ResNet18(pretrained=True, requires_grad=True),
-               ResNet18_v2(pretrained=True, requires_grad=True)]
+               ResNet18_v2(pretrained=True, requires_grad=True),
+               ResNet18_4ch(pretrained=False, requires_grad=True)]
 loss_list = [nn.MSELoss()]
 
 
@@ -370,7 +423,8 @@ if __name__ == "__main__":
 
     x = torch.randn(4,4,256,256) # testing the output
 
-    net = models_list[0]
+    net = models_list[6]
+    print(net)
 
     # print(type(loss_list[0]))
     # print(type(loss_list[1]))

src/train.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 
 import wandb
 
-def training_loop(config, device, img_list, ann_list, train_transform, valid_transform):
+def training_loop(config, device, img_list, ann_list, mask_list, train_transform, valid_transform):
 
     
     _, _, training = utils.first_layer_keys(config)
@@ -25,11 +25,11 @@ def training_loop(config, device, img_list, ann_list, train_transform, valid_tra
 
         print('Fold {}'.format(fold + 1))
 
-        train_img, train_ann = utils.index_with_list(img_list, ann_list, train_index)
-        valid_img, valid_ann = utils.index_with_list(img_list, ann_list, valid_index)
+        train_img, train_ann, train_mask = utils.index_with_list(img_list, ann_list, mask_list, train_index)
+        valid_img, valid_ann, valid_mask = utils.index_with_list(img_list, ann_list, mask_list, valid_index)
 
-        train_set = loader.KeypointsDataset(config, train_ann, train_img, train_transform)
-        valid_set = loader.KeypointsDataset(config, valid_ann, valid_img, valid_transform)
+        train_set = loader.KeypointsDataset(config, train_ann, train_img, train_mask, train_transform, training['depth'])
+        valid_set = loader.KeypointsDataset(config, valid_ann, valid_img, valid_mask, valid_transform, training['depth'])
 
         train_loader = torch.utils.data.DataLoader(train_set, batch_size=training['batch'])
         valid_loader = torch.utils.data.DataLoader(valid_set, batch_size=training['batch'])
@@ -125,7 +125,7 @@ def _log_losses(config, all_losses): # FIXME: this is the final boss of nightmar
     print('Performance of {} fold cross validation'.format(k))
     print("Average Training Loss: {:.5f} \t Average Test Loss: {:.5f}".format(train_folds_average,valid_folds_average))
     
-    best_fold_idx = valid_folds.index(min(valid_folds))
+    best_fold_idx = valid_folds.index(min(valid_folds))+1
     average_fold_idx = utils.closest_to_average(valid_folds_average, valid_folds)
 
     print(f'Best performing epoch: {best_fold_idx}\nEpoch closest to average: {average_fold_idx}')
@@ -147,8 +147,7 @@ def _log_losses(config, all_losses): # FIXME: this is the final boss of nightmar
     
     utils.dump_to_json(loss_dictionary,os.path.join(config['folders']['out_folder'],f"loss_{config['training']['checkpoint_name']}.json"))
 
-    return loss_dictionary # FIXME: how do you get over pasing the same shit from the function inception???
-
+    return loss_dictionary 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
     def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):